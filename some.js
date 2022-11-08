32b3471e4180d625dc7e981e4980f974d992d858
boob.yang <95600496+yangbobo2021@users.noreply.github.com>
2022-11-08 11:05:58 +0800

    add test

--- some.js
+++ some.js
@@ -4,13 +4,16 @@ function somejs() {
   return 20;
 }
 
-function somejs2() {
-  return somejs() + 25;
-}
-
 function somejsx1() {
   return 20;
 }
+
+function somejs2() {
+  somejsx1();
+  return somejs() + 25;
+}
+
+
 
 function somejs3() {
   return somejs() + 35;
